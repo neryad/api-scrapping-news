eee7860070a9d9da5cfeb094d89c26cfcf58a860
Dayern Gomez <dayern.gomez@hotmail.com>
2024-03-28 08:20:28 -0400

    Update diarioLibre.js

--- controllers/diarioLibre.js
+++ controllers/diarioLibre.js
@@ -4,17 +4,27 @@ const cheerio = require('cheerio');
 const getDiarioLibreNews = async (req, res, next) => {
   let articles = [];
 
-  const mediaUrl = 'https://www.diariolibre.com/';
+  const mediaUrl = 'https://www.diariolibre.com/ultima-hora';
   try {
-    const { data: html } = await axios.get(mediaUrl);
-    const $ = cheerio.load(html);
-    $('.headline', html).each(function () {
-      let text = $(this).find('.priority-content');
+    // const data = await axios.get(mediaUrl);
+    // console.log(data.data);
+    // return;
+    const data = await axios.get(mediaUrl);
+    // console.log(data.data);
+    const $ = cheerio.load(data.data);
+
+    $('.w-full', data.data).each(function () {
+      let text = $(this).find('.mb-3');
+      // console.log(text, 'text');
       const title = text.text();
-      const url = 'https://www.diariolibre.com' + $(this).find('a').attr('href');
-      let sourceImg = $(this).find('img').attr('data-srcset');
+      // const url = 'https://www.diariolibre.com/' + $(this).find('.mb-3').filter('a').attr('href');
+      const url = 'https://www.diariolibre.com' + $('h2 a').attr('href');
+      //console.log(url, 'url');
+      let sourceImg = $(this).find('img.mb-3').attr('data-src');
+      //let sourceImg = $('a img').attr('data-src');
+      // console.log(sourceImg);
       if (!sourceImg) {
-        sourceImg = 'https://www.tbh-location.fr/wp-content/uploads/2019/12/news-e1544436032461.png';
+        sourceImg = './assets/news.png';
       }
       let clearStepOne = sourceImg.replace(' 620w', '');
       let clearStepTwo = clearStepOne.replace(' 980w', '');
@@ -30,3 +40,37 @@ const getDiarioLibreNews = async (req, res, next) => {
 };
 
 module.exports = { getDiarioLibreNews };
+
+
+// const axios = require('axios');
+// const cheerio = require('cheerio');
+
+// const getDiarioLibreNews = async (req, res, next) => {
+//   let articles = [];
+
+//   const mediaUrl = 'https://www.diariolibre.com/';
+//   try {
+//     const { data: html } = await axios.get(mediaUrl);
+//     const $ = cheerio.load(html);
+//     $('.headline', html).each(function () {
+//       let text = $(this).find('.priority-content');
+//       const title = text.text();
+//       const url = 'https://www.diariolibre.com' + $(this).find('a').attr('href');
+//       let sourceImg = $(this).find('img').attr('data-srcset');
+//       if (!sourceImg) {
+//         sourceImg = 'https://www.tbh-location.fr/wp-content/uploads/2019/12/news-e1544436032461.png';
+//       }
+//       let clearStepOne = sourceImg.replace(' 620w', '');
+//       let clearStepTwo = clearStepOne.replace(' 980w', '');
+//       let clearStepThree = clearStepTwo.replace(' 619w', '');
+//       const img = clearStepThree;
+//       articles.push({ title, url, img });
+//     });
+//     res.json({ ok: true, data: articles });
+//   } catch (error) {
+//     console.log(error);
+//     res.json({ ok: false, data: 'Error' });
+//   }
+// };
+
+// module.exports = { getDiarioLibreNews };
