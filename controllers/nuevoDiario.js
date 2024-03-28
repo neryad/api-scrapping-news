2b5abfc4f579cfb2e97eed55a9b8aee51bc8b617
Dayern Gomez <dayern.gomez@hotmail.com>
2024-03-28 11:15:27 -0400

    Update nuevoDiario.js

--- controllers/nuevoDiario.js
+++ controllers/nuevoDiario.js
@@ -4,29 +4,45 @@ const cheerio = require('cheerio');
 const getNuevoDiarioNews = async (req, res, next) => {
   let articles = [];
   //const mediaUrl = 'https://elnuevodiario.com.do/';
-  const mediaUrl = 'https://elnuevodiario.com.do/viral/';
+  const mediaUrl = 'https://elnuevodiario.com.do';
 
   try {
     const { data: html } = await axios.get(mediaUrl);
     const $ = cheerio.load(html);
     $('.mb-4', html).each(function () {
-      // let title = $(this).find('a').attr('title');
-      // const url = $(this).find('a').attr('href');
-      // const img = $(this).find('div').attr('data-src');
-      const title = $('.title').text().trim();
-      const url = $('.title').attr('href');
-      const img = $('img').attr('src');
+      let title = $(this).find('.title').text();
+      const url = $(this).find('.title').attr('href');
+      // const img = $(this).find('a').attr('src');
+      const img = $(this).find('.img-fluid').attr('data-src') || $(this).find('.img-fluid').attr('src');
+      console.log(img, 'img');
+      // const title = $('.title').text().trim();
+      // const url = $('.title').attr('href');
+      //const img = $('.img-fluid').attr('src');
 
       articles.push({ title, url, img });
+      articles = articles.filter((article) => article.title.trim() !== '');
     });
     res.json({ ok: true, data: articles });
   } catch (error) {
     console.log(error);
     res.json({ ok: false, data: 'Error' });
   }
+
+  function removeEmptyAndDuplicateTitles(articles) {
+    const uniqueTitles = new Set();
+    return articles.filter((article) => {
+      const trimmedTitle = article.title.trim();
+      if (trimmedTitle === '' || uniqueTitles.has(trimmedTitle)) {
+        return false;
+      }
+      uniqueTitles.add(trimmedTitle);
+      return true;
+    });
+  }
 };
 
 module.exports = { getNuevoDiarioNews };
+
 
 
 
