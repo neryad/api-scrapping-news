8b8375d252728c530a4d263c6f8009c2ff325610
Dayern Gomez <dayern.gomez@hotmail.com>
2024-03-28 10:43:31 -0400

    Update nuevoDiario.js

--- controllers/nuevoDiario.js
+++ controllers/nuevoDiario.js
@@ -3,15 +3,19 @@ const cheerio = require('cheerio');
 
 const getNuevoDiarioNews = async (req, res, next) => {
   let articles = [];
-  const mediaUrl = 'https://elnuevodiario.com.do/';
+  //const mediaUrl = 'https://elnuevodiario.com.do/';
+  const mediaUrl = 'https://elnuevodiario.com.do/viral/';
 
   try {
     const { data: html } = await axios.get(mediaUrl);
     const $ = cheerio.load(html);
-    $('.image-news', html).each(function () {
-      let title = $(this).find('a').attr('title');
-      const url = $(this).find('a').attr('href');
-      const img = $(this).find('div').attr('data-src');
+    $('.mb-4', html).each(function () {
+      // let title = $(this).find('a').attr('title');
+      // const url = $(this).find('a').attr('href');
+      // const img = $(this).find('div').attr('data-src');
+      const title = $('.title').text().trim();
+      const url = $('.title').attr('href');
+      const img = $('img').attr('src');
 
       articles.push({ title, url, img });
     });
@@ -23,3 +27,31 @@ const getNuevoDiarioNews = async (req, res, next) => {
 };
 
 module.exports = { getNuevoDiarioNews };
+
+
+
+// const axios = require('axios');
+// const cheerio = require('cheerio');
+
+// const getNuevoDiarioNews = async (req, res, next) => {
+//   let articles = [];
+//   const mediaUrl = 'https://elnuevodiario.com.do/';
+
+//   try {
+//     const { data: html } = await axios.get(mediaUrl);
+//     const $ = cheerio.load(html);
+//     $('.image-news', html).each(function () {
+//       let title = $(this).find('a').attr('title');
+//       const url = $(this).find('a').attr('href');
+//       const img = $(this).find('div').attr('data-src');
+
+//       articles.push({ title, url, img });
+//     });
+//     res.json({ ok: true, data: articles });
+//   } catch (error) {
+//     console.log(error);
+//     res.json({ ok: false, data: 'Error' });
+//   }
+// };
+
+// module.exports = { getNuevoDiarioNews };
