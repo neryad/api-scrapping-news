310a5f102e258ba6292ac210ccee33aea1276930
Neryad <dayern.gomez@hotmail.com>
2024-04-02 14:35:48 -0400

    fix on nacional img url

--- controllers/nacional.js
+++ controllers/nacional.js
@@ -15,11 +15,12 @@ const getNacionalNews = async (req, res, next) => {
     }
     const html = await response.text();
     const $ = cheerio.load(html);
+    console.log(html);
 
     $('.utf_post_block_style').each(function () {
       const title = $(this).find('a').text();
       const url = $(this).find('a').attr('href');
-      let img = $(this).find('img').attr('src') ||
+      let img = $(this).find('img').attr('data-src') ||
         'https://raw.githubusercontent.com/neryad/api-scrapping-news/master/assets/news.png';
 
       articles.push({ title, url, img });
