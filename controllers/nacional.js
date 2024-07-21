c9131e95605f8809f85a5efb86a3562284554205
Neryad <dayern.gomez@hotmail.com>
2024-07-21 17:19:32 -0400

    fix: :bug: fix bug on img
    fix the wat that image was scrapping fot el nacional

--- controllers/nacional.js
+++ controllers/nacional.js
@@ -1,27 +1,28 @@
-
-const cheerio = require('cheerio');
+const cheerio = require("cheerio");
 
 const getNacionalNews = async (req, res, next) => {
   let articles = [];
-  const mediaUrl = 'https://elnacional.com.do/';
+  const mediaUrl = "https://elnacional.com.do/";
   try {
-
     const response = await fetch(mediaUrl);
 
     if (!response.ok) {
-
       //throw new Error(`Error al obtener la página: ${response.statusText}`);
-      return res.json({ status: response.status, ok: false, error: response.error });
+      return res.json({
+        status: response.status,
+        ok: false,
+        error: response.error,
+      });
     }
     const html = await response.text();
     const $ = cheerio.load(html);
 
-
-    $('.utf_post_block_style').each(function () {
-      const title = $(this).find('.utf_post_title').text();
-      const url = $(this).find('.utf_post_title a').attr('href');
-      let img = $(this).find('img').attr('data-src') ||
-        'https://raw.githubusercontent.com/neryad/api-scrapping-news/master/assets/news.png';
+    $(".utf_post_block_style").each(function () {
+      const title = $(this).find(".utf_post_title").text();
+      const url = $(this).find(".utf_post_title a").attr("href");
+      let img =
+        $(this).find("img").attr("src") ||
+        "https://raw.githubusercontent.com/neryad/api-scrapping-news/master/assets/news.png";
 
       articles.push({ title, url, img });
     });
